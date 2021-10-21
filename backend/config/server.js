1602996b83d1878139f8da31604023e9529d00ef
arnauamargant <Sp18Jv93>
2021-10-21 14:12:41 +0200

    ebe

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '71e365634ac4f9123dc409a043f139da'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8e369ef-ccdf-4ed9-9f0e-13f0d8352c0a'
+};
